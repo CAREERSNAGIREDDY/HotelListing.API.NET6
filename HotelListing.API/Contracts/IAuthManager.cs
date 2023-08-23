8775c13010bc7665162812810de3d0670b2b25d2
Win10 pro <Win10 pro@DESKTOP-NCMHTMJ>
2023-08-23 09:27:33 +0530

    Setup Auth Manager for registration

---
+++ HotelListing.API/Contracts/IAuthManager.cs
@@ -0,0 +1,11 @@
+﻿using HotelListing.API.Models.Hotel.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.API.Contracts
+{
+    public interface IAuthManager
+    {
+        //Task<bool> Register(ApiUserDto userDto);
+        Task<IEnumerable<IdentityError>> Register(ApiUserDto userDto);
+    }
+}
